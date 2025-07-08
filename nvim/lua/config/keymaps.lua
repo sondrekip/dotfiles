18528ff11568aa64b6b394ab029f8a70171f6384
sondre k <sondrek@pc-658.home>
2025-07-08 13:24:35 +0200

    feat: move nvim inside .config

--- nvim/lua/config/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-vim.keymap.set("i", "jj", "<Esc>", { silent = true })
-vim.keymap.set("i", "jk", "<Esc>", { silent = true })
-vim.keymap.set("t", "jj", "<C-\\><C-n>:q<CR>", { silent = true })
-vim.keymap.set("t", "jk", "<C-\\><C-n>:q<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<leader>z", ':lua require("lint").try_lint()<CR>', { noremap = true, silent = true })
